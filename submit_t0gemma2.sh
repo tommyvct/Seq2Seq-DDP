384fbe64051354a2259e2c32572a557414b3c024
tommyvct <7903172+tommyvct@users.noreply.github.com>
2026-03-11 23:30:48 -0700

    be conservative

--- submit_t0gemma2.sh
+++ submit_t0gemma2.sh
@@ -21,8 +21,8 @@ JID_INST_4B=$(sbatch --parsable <<'EOT'
 cd $HOME/scratch/Seq2Seq-DDP
 source slurm/init_hpc.sh
 export OMP_NUM_THREADS=7
-# 4b label = 8B actual (4B enc + 4B dec). Effective batch: 16 * 8 GPUs * 8 accum = 1024
-torchrun --nproc_per_node=8 inst_tuning_t0gemma2.py --model_size "4b" --batch_size 16 --gradient_accumulation_steps 8 --num_workers 7 --seed 27
+# 4b label = 8B actual (4B enc + 4B dec). Effective batch: 24 * 8 GPUs * 5 accum = 960 (~1024)
+torchrun --nproc_per_node=8 inst_tuning_t0gemma2.py --model_size "4b" --batch_size 24 --gradient_accumulation_steps 5 --num_workers 7 --seed 27
 EOT
 )
 
@@ -40,8 +40,8 @@ JID_INST_1B=$(sbatch --parsable <<'EOT'
 cd $HOME/scratch/Seq2Seq-DDP
 source slurm/init_hpc.sh
 export OMP_NUM_THREADS=7
-# 1b label = 2B actual (1B enc + 1B dec). Effective batch: 64 * 8 GPUs * 2 accum = 1024
-torchrun --nproc_per_node=8 inst_tuning_t0gemma2.py --model_size "1b" --batch_size 64 --gradient_accumulation_steps 2 --num_workers 7 --seed 27
+# 1b label = 2B actual (1B enc + 1B dec). Effective batch: 128 * 8 GPUs * 1 accum = 1024
+torchrun --nproc_per_node=8 inst_tuning_t0gemma2.py --model_size "1b" --batch_size 128 --gradient_accumulation_steps 1 --num_workers 7 --seed 27
 EOT
 )
 
