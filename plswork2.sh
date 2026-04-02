18ae7623f9e3ff8c6df73b94b827be3701a82356
tommyvct <7903172+tommyvct@users.noreply.github.com>
2026-04-01 18:40:47 -0700

    amend

--- plswork2.sh
+++ plswork2.sh
@@ -16,9 +16,9 @@ cd $SCRATCH/Seq2Seq-DDP
 source slurm/init_hpc.sh
 python3 train.py --train_corpus molweni --do_train -s natural2 -t t5gemma2 -m 4b -l 5e-6 -e 3 --batchsize 4 --step 2000 -b
 echo "Training Complete"
-python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b  --lr 5e-6 --batchsize 4 --step 2000 --seed 27 -b
+python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 3 --batchsize 4 --step 2000 --seed 27 -b
 echo "Inference Complete"
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6  --batchsize 4 --step 2000 --seed 27 > eval/t5gemma2-4b_5e-6_e3_b4_s2000_molweni_natural2.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 -e 3 --batchsize 4 --step 2000 --seed 27 > eval/t5gemma2-4b_5e-6_e3_b4_s2000_molweni_natural2.txt
 EOF
 
 # Job 1: New Prompt, Default Batch Size (4)
@@ -40,10 +40,10 @@ source slurm/init_hpc.sh
 python3 train.py --train_corpus molweni --do_train -s natural2 -t t5gemma2 -m 4b -l 5e-6 -e 5 --batchsize 4 --step 2000 --new_prompt -b
 echo "Training Complete"
 
-python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 --batchsize 4 --step 2000 --seed 27 --new_prompt -b
+python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 4 --step 2000 --seed 27 --new_prompt -b
 echo "Inference Complete"
 
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 --batchsize 4 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b4_s2000_molweni_natural2_newprompt.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 -e 5 --batchsize 4 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b4_s2000_molweni_natural2_newprompt.txt
 INNER_EOF
 
 # Job 2: High Batch Size (16), Default Prompt
@@ -65,10 +65,10 @@ source slurm/init_hpc.sh
 python3 train.py --train_corpus molweni --do_train -s natural2 -t t5gemma2 -m 4b -l 5e-6 -e 5 --batchsize 16 --step 2000 -b
 echo "Training Complete"
 
-python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 --batchsize 16 --step 2000 --seed 27 -b
+python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 -b
 echo "Inference Complete"
 
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 --batchsize 16 --step 2000 --seed 27 > eval/t5gemma2-4b_5e-6_e5_b16_s2000_molweni_natural2.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 > eval/t5gemma2-4b_5e-6_e5_b16_s2000_molweni_natural2.txt
 INNER_EOF
 
 # Job 3: High Batch Size (16) AND New Prompt
@@ -90,10 +90,10 @@ source slurm/init_hpc.sh
 python3 train.py --train_corpus molweni --do_train -s natural2 -t t5gemma2 -m 4b -l 5e-6 -e 5 --batchsize 16 --step 2000 --new_prompt -b
 echo "Training Complete"
 
-python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 --batchsize 16 --step 2000 --seed 27 --new_prompt -b
+python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 --new_prompt -b
 echo "Inference Complete"
 
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 --batchsize 16 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b16_s2000_molweni_natural2_newprompt.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b16_s2000_molweni_natural2_newprompt.txt
 INNER_EOF
 
 
@@ -116,7 +116,7 @@ python3 train.py --train_corpus molweni --do_train -s natural2 -t t5gemma2 -m 4b
 echo "Training Complete"
 python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b  --lr 5e-6  -e 3 --batchsize 8 --step 2000 --seed 27 -b 
 echo "Inference Complete"
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 --seed 27 > eval/t5gemma2-4b_5e-6_e3_b8_molweni_natural2.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 -e 3 --batchsize 8 --step 2000 --seed 27 > eval/t5gemma2-4b_5e-6_e3_b8_s2000_molweni_natural2.txt
 EOF
 
 sbatch << 'EOF'
@@ -137,7 +137,7 @@ python3 train.py --train_corpus molweni --do_train -s natural2 -t t5gemma2 -m 4b
 echo "Training Complete"
 python3 transition_predict.py --train_corpus molweni --test_corpus molweni -s natural2 -t t5gemma2 -m 4b  --lr 5e-6  -e 3 --batchsize 8 --step 2000 --seed 27 -b --new_prompt
 echo "Inference Complete"
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e3_b8_molweni_natural2_newprompt.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus molweni --test_corpus molweni -s natural2 --lr 5e-6 -e 3 --batchsize 8 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e3_b8_s2000_molweni_natural2_newprompt.txt
 EOF
 
 # Next stop, try epoch 7
