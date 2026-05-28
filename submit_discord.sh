7b100942494ac020a0127dc335320e9b55d18753
Tommy Wu <7903172+tommyvct@users.noreply.github.com>
2026-05-28 10:39:00 -0700

    can specify seed

--- submit_discord.sh
+++ submit_discord.sh
@@ -1,8 +1,10 @@
-sbatch << 'INNER_EOF'
+SEED=${1:-27}
+
+sbatch << INNER_EOF
 #!/bin/bash
-#SBATCH --job-name=t5gemma2-4b_train_discord-unveiled_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
-#SBATCH --output=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.out
-#SBATCH --error=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.err
+#SBATCH --job-name=t5gemma2-4b_train_discord-unveiled_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
+#SBATCH --output=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.out
+#SBATCH --error=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.err
 #SBATCH --time=3-0:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
@@ -10,23 +12,23 @@ sbatch << 'INNER_EOF'
 #SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --mail-user=wut2@unbc.ca
 
-cd $SCRATCH/Seq2Seq-DDP
+cd \$SCRATCH/Seq2Seq-DDP
 source slurm/init_hpc.sh
 
 python3 train.py --train_corpus discord-unveiled --do_train -s natural2 -t t5gemma2 -m 4b -l 5e-6 -e 5 --batchsize 16 --step 2000 --new_prompt -b --custom_model_dir ft-models/t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
 echo "Training Complete"
 
-python3 transition_predict.py --train_corpus discord-unveiled --test_corpus discord-unveiled -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 --new_prompt -b --custom_model_dir ft-models/t5gemma2-4b_train_discord-unveiled_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
+python3 transition_predict.py --train_corpus discord-unveiled --test_corpus discord-unveiled -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 32 --step 2000 --seed ${SEED} --new_prompt -b --custom_model_dir ft-models/t5gemma2-4b_train_discord-unveiled_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
 echo "Inference Complete"
 
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus discord-unveiled --test_corpus discord-unveiled -s natural2 --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b16_s2000_discord-unveiled_natural2_newprompt_FROM_molweni.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus discord-unveiled --test_corpus discord-unveiled -s natural2 --lr 5e-6 -e 5 --batchsize 32 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b32_s2000_discord-unveiled_natural2_newprompt_FROM_molweni.txt
 INNER_EOF
 
-sbatch << 'INNER_EOF'
+sbatch << INNER_EOF
 #!/bin/bash
-#SBATCH --job-name=t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
-#SBATCH --output=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.out
-#SBATCH --error=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.err
+#SBATCH --job-name=t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
+#SBATCH --output=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.out
+#SBATCH --error=slurm/logs/%j_t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt.err
 #SBATCH --time=3-0:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
@@ -34,14 +36,14 @@ sbatch << 'INNER_EOF'
 #SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --mail-user=wut2@unbc.ca
 
-cd $SCRATCH/Seq2Seq-DDP
+cd \$SCRATCH/Seq2Seq-DDP
 source slurm/init_hpc.sh
 
 python3 train.py --train_corpus discord-unveiled-hintswap --do_train -s natural2 -t t5gemma2 -m 4b -l 5e-6 -e 5 --batchsize 16 --step 2000 --new_prompt -b --custom_model_dir ft-models/t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
 echo "Training Complete"
 
-python3 transition_predict.py --train_corpus discord-unveiled-hintswap --test_corpus discord-unveiled-hintswap -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 --new_prompt -b --custom_model_dir ft-models/t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed27_5e-6_e5_b16_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
+python3 transition_predict.py --train_corpus discord-unveiled-hintswap --test_corpus discord-unveiled-hintswap -s natural2 -t t5gemma2 -m 4b --lr 5e-6 -e 5 --batchsize 32 --step 2000 --seed ${SEED} --new_prompt -b --custom_model_dir ft-models/t5gemma2-4b_train_discord-unveiled-hintswap_natural2_seed${SEED}_5e-6_e5_b32_s2000_newprompt_FROM_t5gemma2-4b_train_molweni_natural2_seed27_5e-6_e5_b16_s2000_newprompt
 echo "Inference Complete"
 
-python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus discord-unveiled-hintswap --test_corpus discord-unveiled-hintswap -s natural2 --lr 5e-6 -e 5 --batchsize 16 --step 2000 --seed 27 --new_prompt > eval/t5gemma2-4b_5e-6_e5_b16_s2000_discord-unveiled-hintswap_natural2_newprompt_FROM_molweni.txt
+python3 eval_gen.py --fted_model t5gemma2-4b --train_corpus discord-unveiled-hintswap --test_corpus discord-unveiled-hintswap -s natural2 --lr 5e-6 -e 5 --batchsize 32 --step 2000 --seed ${SEED} --new_prompt > eval/t5gemma2-4b_5e-6_e5_b32_s2000_discord-unveiled-hintswap_natural2_newprompt_FROM_molweni.txt
 INNER_EOF
